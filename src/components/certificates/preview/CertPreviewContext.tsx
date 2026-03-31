80afebb159463bbee7a15c5438eb7f22554f4752
Masio84 <masio84@users.noreply.github.com>
2026-03-31 00:21:28 -0600

    fix(certificates): corregir ruta de importación de tipos en CertPreviewContext

--- src/components/certificates/preview/CertPreviewContext.tsx
+++ src/components/certificates/preview/CertPreviewContext.tsx
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
-import { CertificateTemplate } from '../../types/certificate-template.types';
+import { CertificateTemplate } from '../types/certificate-template.types';
 
 export const CertPreviewTemplateContext = createContext<CertificateTemplate | null>(null);
 export const CertPreviewDataContext = createContext<any>(null);
